36b09294907017b2c19ce7367b241fd9c10dde78
philip <optionabc@gmail.com>
2020-01-25 15:17:42 -0500

    Signed-off-by: philip <optionabc@gmail.com>

---
+++ ws3/ws3/PairSummable.cpp
@@ -0,0 +1,20 @@
+//-------------------------------------------------------------------------------------------------------------
+// Name:    Haibo Zheng
+// Seneca Student ID:   164732182
+// Seneca email:    hzheng62@myseneca.ca
+// Date of completion:  1/25/2020
+//
+// I confirm that the content of this file is created by me,  with the exception of the parts provided to me by my professor.
+//-------------------------------------------------------------------------------------------------------------
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include "PairSummable.h"
+#include "SetSummable.h"
+
+using namespace std;
+
+namespace sdds {
+
+}
---
+++ ws3/ws3/Set.h
@@ -0,0 +1,24 @@
+//-------------------------------------------------------------------------------------------------------------
+// Name:    Haibo Zheng
+// Seneca Student ID:   164732182
+// Seneca email:    hzheng62@myseneca.ca
+// Date of completion:  1/25/2020
+//
+// I confirm that the content of this file is created by me, with the exception of the parts provided to me by my professor.
+//-------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef _SDDS_SET_HEADER_
+#define _SDDS_SET_HEADER_
+
+
+namespace sdds {
+    class Set {
+
+    };
+}
+#endif
