d3327b5bbd20656c94ee0b4e100a5dbfc32811b5
philip <optionabc@gmail.com>
2020-02-06 10:38:17 -0500

    1

---
+++ oop 244 2020 winter/ws03/Train.h
@@ -0,0 +1,53 @@
+//==============================================
+// Name:           Xiaozhuan Li
+// Student Number: 162683189
+// Email:          xli403@myseneca.ca
+// Date:           Jan-28-2020
+// Section:        OOP244NAA
+// Workshop:       3 (diy)
+//==============================================
+// Train.h
+
+#ifndef SDDS_TRAIN_H 
+#define SDDS_TRAIN_H 
+
+namespace sdds {
+	const int MAX_NAME = 30;
+	const int MAX_DESC = 20;
+	const double MAX_WEIGHT = 700.555;
+	const double MIN_WEIGHT = 40.444;
+
+	class Cargo {
+		char* m_description{};
+		char m_descriptionArray[MAX_DESC]{};
+
+		double m_weight{};
+	public:
+		void setDescription(const char* s_description);
+		void setDescriptionPtr(char* s_description);
+		void setWeight(const double s_double);
+		char* getDescription() const;
+		char* getDescriptionArray() ;
+		double getWeight() const;
+		void deleteCargo();
+		void init(const char*, double);
+		void initPtr( char*, double);
+	};
+
+	class Train {
+		char m_name[MAX_NAME + 1]{};
+		int m_id{};
+		Cargo* m_cargo{};
+	public:
+		void setTrain(const char*, int);
+		bool isEmpty() const;
+		void display() const;
+		void loadCargo(Cargo);
+		void unloadCargo();
+		
+		bool swapCargo(Train&);
+		bool increaseCargo(double);
+		bool decreaseCargo(double);
+	};
+}
+#endif
